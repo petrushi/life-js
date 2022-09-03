0dc487d7ed0cb0621f3755999dd864a4f8afb4f6
petrushi <pavlovinthesky@gmail.com>
2022-09-03 17:10:45 +0300

    rm python

--- check.py
+++
@@ -1,8 +0,0 @@
-for x in range(1,100):
-    for y in range(20,100):
-        summed = x+y
-        multiplied = x*y
-        if multiplied - summed == 2500:
-            print(x, y)
-            break
-            
